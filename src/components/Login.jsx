0c0d2345e122330ad4a900d08a02ceb89270d852
ProgrammingWithMuzamil <muhmmadmuzamil445@gmail.com>
2026-01-31 05:56:34 +0500

    add

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -88,7 +88,7 @@ const Login = () => {
               </button>
             </div>
 
-            <div className="mt-6">
+            {/* <div className="mt-6">
               <div className="relative">
                 <div className="absolute inset-0 flex items-center">
                   <div className="w-full border-t border-gray-300" />
@@ -106,7 +106,7 @@ const Login = () => {
                   Create an account
                 </Link>
               </div>
-            </div>
+            </div> */}
           </form>
         </div>
       </div>
