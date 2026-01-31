571095d63fd21ee87558b58498af8a74c5fbc6d1
ProgrammingWithMuzamil <muhmmadmuzamil445@gmail.com>
2026-01-31 06:10:56 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { useAuth } from './contexts/AuthContext.jsx';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Login from './components/Login.jsx';
-import Signup from './components/Signup.jsx';
 import Layout from './components/Layout.jsx';
 import Dashboard from './components/Dashboard.jsx';
 import Users from './pages/Users.jsx';
@@ -57,14 +56,6 @@ function App() {
             element={
               <PublicRoute>
                 <Login />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="/signup"
-            element={
-              <PublicRoute>
-                <Signup />
               </PublicRoute>
             }
           />
